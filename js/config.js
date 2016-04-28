4656f4e9d1c82e5351e075ad163039b4716f95c8
Henson <henson.lu@gmail.com>
2016-04-28 23:23:08 +0800

    fix

--- js/config.js
+++ js/config.js
@@ -1,19 +1,19 @@
 var config = {
-    lang: 'zh-cn',
+    lang: "zh-cn",
     time: {
         timeFormat: 24
     },
     weather: {
         params: {
-            cityid: 'CN101240101',
-            key: 'b510b5ef572b40d5b60831f87afac101'
+            cityid: "",
+            key: ""
         }
     },
     tem_hum: {
         mqttServer:"localhost",
-	mqttServerPort:9001,
-	mqttclientName:"magic_mirror_tem_hum",
-	temperatureTopic:"/DHT"
+		mqttServerPort:9001,
+		mqttclientName:"magic_mirror_tem_hum",
+		temperatureTopic:"/DHT"
     },
     compliments: {
         interval: 30000,
@@ -36,7 +36,7 @@ var config = {
     },
     calendar: {
         maximumEntries: 10,
-        url: "https://p04-calendarws.icloud.com/ca/subscribe/1/xsnGWRhKpdsetOOuMVsj-Y9bNbZMz7NhFf7TC88ObsdSiN5nheXud4szxoLCON1O"
+        url: ""
     },
     news: {
         feed: 'http://www.ftchinese.com/rss/news'
--- script/weather.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys, urllib, urllib2, json
-url = 'https://api.heweather.com/x3/weather?cityid=CN101240101&key=b510b5ef572b40d5b60831f87afac101'
-req = urllib2.Request(url)
-resp = urllib2.urlopen(req)
-content = resp.read()
-if(content):
-	print(content)
